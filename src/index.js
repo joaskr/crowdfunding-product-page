d43a7a6f22d140e10cab73a73b6bff74a66ed41f
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2023-01-23 23:07:31 +0100

    Added reset.scss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./utilities/_reset.scss";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
